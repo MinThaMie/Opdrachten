6ee5b2de4bf246eb14e101c26d5a2752056976ce
Jasper Gerth <jaspergerth@gmail.com>
2015-12-13 16:06:31 +0100

    starting with arrays for players

--- softwaresystems/src/ss/week5/TicTacToe/TicTacToe.java
+++ softwaresystems/src/ss/week5/TicTacToe/TicTacToe.java
@@ -11,8 +11,14 @@ public class TicTacToe {
 	private static Player player1;
 	private static Player player2;
 	private static Game spel;
+	private static final String NAIVEPC="-N";
 	
 	public static void main(String[] args) {
+		for (String arg : args){
+			if (arg.equals(NAIVEPC))
+				
+				
+		}
 		 player1 = new HumanPlayer(args[0], Mark.OO);
 		 player2 = new HumanPlayer(args[1], Mark.XX);
 

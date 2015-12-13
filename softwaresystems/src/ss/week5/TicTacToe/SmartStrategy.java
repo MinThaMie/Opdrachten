a5bddfe4f086e1c57a4feb55f378210326b5650e
Jasper Gerth <jaspergerth@gmail.com>
2015-12-13 20:01:42 +0100

    finished implementation of smart strategy

---
+++ softwaresystems/src/ss/week5/TicTacToe/SmartStrategy.java
@@ -0,0 +1,81 @@
+package ss.week5.TicTacToe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SmartStrategy implements Strategy {
+	private static final String NAME = "SmartStrategy";
+	private static final int MIDDLE = 4;
+
+	public SmartStrategy() {
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public int determineMove(Board b, Mark m) {
+
+		int result;
+		if (copy(b).isEmptyField(MIDDLE)) {
+			result = MIDDLE;
+		} else if (hasDirectWin(copy(b), m)) {
+			result = isDirectWin(copy(b), m);
+		} else if (hasDirectWin(copy(b), m.other())) {
+			result = isDirectWin(copy(b), m.other());
+		} else {
+			List<Integer> emptyFields = emptyFields(b);
+			result = (int) emptyFields.get((int) Math.floor(Math.random() * (emptyFields.size())));
+		}
+
+	}
+
+	private Board copy(Board b) {
+		return b.deepCopy();
+	}
+
+	private boolean hasDirectWin(Board b, Mark m) {
+		boolean hasWin = false;
+		Mark oldMark;
+		for (int i = 0; i < (Board.DIM * Board.DIM); i++) {
+			oldMark = b.getField(i);
+			b.setField(i, m);
+			if (b.isWinner(m)) {
+				hasWin = true;
+				break;
+			}
+			b.setField(i, oldMark);
+		}
+		return hasWin;
+	}
+
+	private int isDirectWin(Board b, Mark m) {
+		int isWin = 0;
+		Mark oldMark;
+		for (int i = 0; i < (Board.DIM * Board.DIM); i++) {
+			oldMark = b.getField(i);
+			b.setField(i, m);
+			if (b.isWinner(m)) {
+				isWin = i;
+				break;
+			}
+			b.setField(i, oldMark);
+		}
+		return isWin;
+
+	}
+
+	private static List<Integer> emptyFields(Board b) {
+		List<Integer> emptyFields = new ArrayList<Integer>();
+		int j = 0;
+		for (int i = 0; i < (Board.DIM * Board.DIM); i++) {
+			if (b.isEmptyField(i)) {
+				emptyFields.add(j, i);
+				j++;
+			}
+		}
+		return emptyFields;
+	}
+}
--- softwaresystems/src/ss/week5/TicTacToe/TicTacToe.java
+++ softwaresystems/src/ss/week5/TicTacToe/TicTacToe.java
@@ -1,5 +1,6 @@
 package ss.week5.TicTacToe;
 
+
 /**
  * Executable class for the game Tic Tac Toe. The game can be played against the
  * computer. Lab assignment Module 2
@@ -10,22 +11,27 @@ package ss.week5.TicTacToe;
 public class TicTacToe {
 	private static Player player1;
 	private static Player player2;
+
 	private static Game spel;
-	private static final String NAIVEPC="-N";
-	
+	private static final String NAIVEPC = "-N";
+
 	public static void main(String[] args) {
-		for (String arg : args){
-			if (arg.equals(NAIVEPC))
-				
-				
+
+		if (args[0].equals(NAIVEPC)) {
+			player1 = new ComputerPlayer(Mark.OO, new NaiveStrategy());}
+		else{
+			player1=new HumanPlayer(args[0],Mark.OO);
 		}
-		 player1 = new HumanPlayer(args[0], Mark.OO);
-		 player2 = new HumanPlayer(args[1], Mark.XX);
+		
+		if (args[1].equals(NAIVEPC)){
+			player2=new ComputerPlayer(Mark.XX, new NaiveStrategy());
+		}else{
+			player2= new HumanPlayer(args[1],Mark.XX);
+		}			
 
 		spel = new Game(player1, player2);
 		spel.start();
 
 	}
-	
-}
 
+}

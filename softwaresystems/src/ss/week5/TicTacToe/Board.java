a81da7d7b95648cd470063d229f73b7a5c97836e
Jasper Gerth <jaspergerth@gmail.com>
2015-12-13 20:28:58 +0100

    Revert "tried to fix alignment"
    This reverts commit cb30a1ff5a9eac99d0eded056533c178c0e79024.

--- softwaresystems/src/ss/week5/TicTacToe/Board.java
+++ softwaresystems/src/ss/week5/TicTacToe/Board.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  */
 public class Board {
 	public static final int DIM = 3;
-	private static final String[] NUMBERING = { " 0 | 1 | 2 ", "-----+-----+-----", " 3 | 4 | 5 ", "-----+-----+-----",
+	private static final String[] NUMBERING = { " 0 | 1 | 2 ", "---+---+---", " 3 | 4 | 5 ", "---+---+---",
 			" 6 | 7 | 8 " };
 	private static final String LINE = NUMBERING[1];
 	private static final String DELIM = "     ";
@@ -301,10 +301,7 @@ public class Board {
 		for (int i = 0; i < DIM; i++) {
 			String row = "";
 			for (int j = 0; j < DIM; j++) {
-				if(getField(i,j)==Mark.EMPTY)
 				row = row + " " + getField(i, j).toString() + " ";
-				else
-					row = row + "   " + getField(i, j).toString() + "   ";
 				if (j < DIM - 1) {
 					row = row + "|";
 				}

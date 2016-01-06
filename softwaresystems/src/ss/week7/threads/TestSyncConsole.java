60312783dda4aeb72165861b180134947ededb8c
Jasper Gerth <jaspergerth@gmail.com>
2016-01-06 13:42:50 +0100

    Made Synced console

---
+++ softwaresystems/src/ss/week7/threads/TestSyncConsole.java
@@ -0,0 +1,31 @@
+package ss.week7.threads;
+
+public class TestSyncConsole extends Thread {
+	//-----------------------------Instance Variables
+	
+
+	//-----------------------------Constructor
+	public TestSyncConsole(String name){
+		super(name);	
+	}
+	
+	public static void main(String[] args){
+		(new TestSyncConsole("A")).start();
+		(new TestSyncConsole("B")).start();		
+	}
+
+	//-----------------------------Queries
+	
+		
+
+	//-----------------------------Commands
+private void sum(){
+int number1 = SyncConsole.readInt( "Thread: " + this.getName() +" input integer 1 ");
+int number2 = SyncConsole.readInt("Thread: " + this.getName() + " input integer 2 ");
+
+SyncConsole.println(number1 + " + " + number2 + " = " + (number1+number2));
+}
+	public void run(){
+		sum();		
+	}
+}
